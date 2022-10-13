9d8440226ac238e7065e256b40632f11f202cdf5
dhirajnikam <dhirajnikam78@gmail.com>
2022-10-13 17:54:41 +0530

    katex

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -84,7 +84,7 @@ class _ExampleState extends State<Example> {
             const SizedBox(
               height: 20,
             ),
-            AtomicPreview(
+            AtomicKatexPreview(
               laTex: changeValue.controller.text,
             )
           ],
